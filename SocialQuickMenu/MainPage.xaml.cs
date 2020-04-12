d51aa6934807a25c671f3bcc733398c4b613e96f
panchanathantharsan <tharsan.3112@gmail.com>
2020-04-12 16:38:59 +0530

    Initial check-in of module SocialQuickMenu

---
+++ SocialQuickMenu/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SocialQuickMenu
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        void close(System.Object sender, System.EventArgs e)
+        {
+            contentPage.Opacity = 1;
+            menuitems.IsVisible = false;
+        }
+
+
+        void TapGestureRecognizer_Tapped_3(System.Object sender, System.EventArgs e)
+        {
+            Image image = sender as Image;
+            string filename = image.Source.ToString();
+            if (filename == "File: close.png")
+            {
+                contentPage.Opacity = 1;
+                menu.Source = "share.png";
+                menulist.IsVisible = false;
+                contentPage.InputTransparent = false;
+            }
+            else if (filename == "File: share.png")
+            {
+                contentPage.Opacity = 0.3;
+                menu.Source = "close.png";
+                menulist.IsVisible = true;
+                contentPage.InputTransparent = true;
+            }
+        }
+
+    }
+}
